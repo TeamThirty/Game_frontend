b60fec0688e6d9d79040fc5ffc9837f8d4177fb4
Kirill Golygin <kirik3007@gmail.com>
2023-11-11 17:20:47 +0300

    create color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Room from "./components/Room/Room";
 
 import s from "./App.module.css";
+import ColorPicker from "./components/Color-picker/Color-picker";
 
 function App() {
   return (
     <main className={s.main}>
       <Room />
+      <ColorPicker />
     </main>
   );
 }
--- src/components/Color-picker/Color-picker.tsx
+++ src/components/Color-picker/Color-picker.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import { useState } from "react";
 
-import { BlockPicker } from "react-color";
+import { ChromePicker, ColorResult, ColorChangeHandler } from "react-color";
 
 const ColorPicker = () => {
-  return <BlockPicker />;
+  const [color, changeColor] = useState<string>();
+
+  const handleChangeComplete: ColorChangeHandler = (color: ColorResult) => {
+    changeColor(color.hex);
+  };
+  return <ChromePicker onChangeComplete={handleChangeComplete} color={color} />;
 };
 
 export default ColorPicker;
